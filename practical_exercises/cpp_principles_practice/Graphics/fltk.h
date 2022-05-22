91e24ecddf4ee73073f377c679bacdc5991c633e
robin <jiangrenbin329@gmail.com>
2022-05-22 21:13:20 +0800

    add Chapter17
    Signed-off-by: robin <jiangrenbin329@gmail.com>

---
+++ practical_exercises/cpp_principles_practice/Graphics/fltk.h
@@ -0,0 +1,28 @@
+//
+// Created by renbin jiang on 2022/5/22.
+//
+
+#ifndef CPLUSPLUSTHINGS_FLTK_H
+#define CPLUSPLUSTHINGS_FLTK_H
+// -----------------------------------------------------------------------------
+#ifndef FLTK_GUARD
+#define FLTK_GUARD 1
+
+// -----------------------------------------------------------------------------
+
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Button.H>
+#include <FL/Fl_Input.H>
+#include <FL/Fl_Output.H>	// for exit(0)
+#include <FL/fl_draw.H>
+#include <FL/Enumerations.H>
+#include <FL/Fl_JPEG_Image.H>
+#include <FL/Fl_GIF_Image.H>
+#include <cstdlib>
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+// -----------------------------------------------------------------------------
+#endif
+
+#endif // CPLUSPLUSTHINGS_FLTK_H
