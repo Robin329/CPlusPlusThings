74e72f0706e5682c965e150be88bbc352ae593b7
robin <jiangrenbin329@gmail.com>
2022-07-17 10:05:49 -0700

    update binary
    Signed-off-by: robin <jiangrenbin329@gmail.com>

--- practical_exercises/key_exercises/31.data_type_range.cpp
+++ practical_exercises/key_exercises/31.data_type_range.cpp
@@ -9,9 +9,9 @@
 #include <float.h>
 
 // 使用bitset 将 int 转换二进制
-void BinaryBitset(int n)
+template <typename T> void BinaryBitset(T n)
 {
-	std::cout << bitset<sizeof(int) * 8>(n) << "b" << std::endl;
+	std::cout << bitset<sizeof(T) * 8>(n) << "b" << std::endl;
 }
 
 int main()
@@ -36,14 +36,20 @@ int main()
 	printf("Range of long double : %e -- %e\n", LDBL_MIN, LDBL_MAX);
 
 	// data type out of range
-	U64 a = 4294967295;
-	U64 b = 0;
+	U64 a = 18446744073709551615;
+	U64 b = 0, c = 0;
 
 	b = a + 5;
-
+	c = a - 2;
 	printf("sizeof(a) = %d\n", sizeof a);
 	printf("sizeof(int) = %d\n", sizeof b);
-	BinaryBitset(b);
+	BinaryBitset<u64>(b);
+	BinaryBitset<u64>(c);
+	BinaryBitset<S64>(-c);
+	// 负数表示为原码的补码 = 对该数的原码(正数的原码是本身，负数是绝对值最高位加1)除符号位外各位取反，然后最后一位加1
+	BinaryBitset<u64>(-c);
 	printf("integer b = %d\n\n", b);
+	std::cout << (b - c) << std::endl;
+	std::cout << (s64)(b - c) << std::endl;
 	return 0;
 }
