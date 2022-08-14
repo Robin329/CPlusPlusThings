1a983c7a85681d128fc72fa5ab3099b04e3d9364
robin <jiangrenbin329@gmail.com>
2022-08-14 06:42:49 -0700

    update crc
    Signed-off-by: robin <jiangrenbin329@gmail.com>

---
+++ practical_exercises/crc/crchash.cpp
@@ -0,0 +1,39 @@
+/*
+** The crc hash is licensed under the Apache License, Version 2.0, and a copy of the license is included in this file.
+**
+**Author:Wang Yaofu voipman@qq.com
+**Description: The source file of class CrcHash.
+*/
+
+#include "crchash.h"
+#include "crc16.h"
+#include "crc32.h"
+#include "crc64.h"
+namespace common {
+    uint16_t Hash16(const std::string& key)
+    {
+        return crc16(key.c_str(), key.size());
+    }
+    uint16_t Hash16(const char* cpKey, const int iKeyLen)
+    {
+        return crc16(cpKey, iKeyLen);
+    }
+
+    uint32_t Hash32(const std::string& key)
+    {
+        return crc32(key.c_str(), key.size());
+    }
+    uint32_t Hash32(const char* cpKey, const int iKeyLen)
+    {
+        return crc32(cpKey, iKeyLen);
+    }
+
+    uint64_t Hash64(const std::string& key)
+    {
+        return crc64(key.c_str(), key.size());
+    }
+    uint64_t Hash64(const char* cpKey, const int iKeyLen)
+    {
+        return crc64(cpKey, iKeyLen);
+    }
+} // namespace common
